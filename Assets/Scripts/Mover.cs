bd4127c30e9fa336c80ed4b2b0f665a89a07daba
Michael Tozzo <mtozzo@gmail.com>
2025-09-03 21:52:52 -0400

    Improves the physics

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
 
     private InputAction moveAction;
+    private Rigidbody rb;
+    private Vector2 moveInput;
 
     private void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.freezeRotation = true;
     }
 
     private void Update()
     {
-        var moveInput = moveAction.ReadValue<Vector2>();
-        transform.Translate(moveInput.x * moveSpeed * Time.deltaTime, 0, moveInput.y * moveSpeed * Time.deltaTime);
+        moveInput = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        rb.linearVelocity = move * moveSpeed;
     }
 }
