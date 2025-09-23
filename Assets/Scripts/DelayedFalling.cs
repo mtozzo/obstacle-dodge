a238b8c5732eca51bbeee90d118bd4b436f40d73
Michael Tozzo <mtozzo@gmail.com>
2025-09-23 00:39:17 -0400

    Fix material references and add a falling object

---
+++ Assets/Scripts/DelayedFalling.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DelayedFalling : MonoBehaviour
+{
+    [SerializeField] private float delay = 3f;
+    private bool gravityEnabled = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (gravityEnabled || !(Time.time > delay)) return;
+        
+        gravityEnabled = true;
+        var rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        Debug.Log("useGravity after delay: " + rb.useGravity);
+        Destroy(this); // Remove this component after enabling gravity
+    }
+}
