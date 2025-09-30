75530c2fbeef17998a67b22bc67a15fe2ecc587a
Michael Tozzo <mtozzo@gmail.com>
2025-09-30 15:25:22 -0400

    Improve projectiles so serialized data is used

--- Assets/Scripts/FlyTowardsPlayer.cs
+++ Assets/Scripts/FlyTowardsPlayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FlyTowardsPlayer : MonoBehaviour
 {
     [SerializeField] private Transform playerPosition;
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed;
 
     private void FixedUpdate()
     {
