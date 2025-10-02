bd32ae68d5087401e2dddfc9e246617a6592ff18
Michael Tozzo <mtozzo@gmail.com>
2025-10-02 00:39:56 -0400

    Improve projectiles so they target a fixed position instead of following

--- Assets/Scripts/FlyTowardsPlayer.cs
+++ Assets/Scripts/FlyTowardsPlayer.cs
@@ -1,12 +1,33 @@
+using System;
 using UnityEngine;
 
 public class FlyTowardsPlayer : MonoBehaviour
 {
     [SerializeField] private Transform playerPosition;
     [SerializeField] private float speed;
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        targetPosition = playerPosition.position;
+    }
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, playerPosition.position, 0.1f * Time.deltaTime * speed);
+        MoveToPlayer();
+        DestroyWhenDestinationReached();
+    }
+
+    private void MoveToPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);        
+    }
+    
+    private void DestroyWhenDestinationReached()
+    {
+        if (transform.position == targetPosition)
+        {
+            Destroy(gameObject);
+        }
     }
 }
